6119dcdafe708e4e675b718ddbe9c6b29ad067c9
Alexander Oplesnin <Alexander Oplesnin>
2016-04-03 11:03:15 +0300

    Planets canvases

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,74 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
+public class Planet : MonoBehaviour
+{
+    public GameObject DefaultSelector;
 
-public class Planet : MonoBehaviour {
+    bool isFocused = false;
 
-	bool isFocused = false;
+    public CanvasGroup canvas;
 
-	public CanvasGroup canvas;
+    public bool IsFocused
+    {
+        get
+        {
+            return isFocused;
+        }
+    }
 
-	public bool IsFocused
-	{
-		get
-		{
-			return isFocused;
-		}
-	}
+    public void Start()
+    {
+        canvas.alpha = 0;
+        canvas.interactable = false;
+    }
 
-	public void Focus()
-	{
-		isFocused = true;
+    public void Focus()
+    {
+        isFocused = true;
         transform.DOKill();
         transform.DOScale(1.1f * Vector3.one, 0.5f);
 
-		canvas.DOKill ();
-		canvas.DOFade (1f, 0.5f);
+        canvas.DOKill();
+        canvas.DOFade(1f, 0.5f);
+        canvas.interactable = true;
+
+        EventSystem.current.SetSelectedGameObject(DefaultSelector);
     }
 
-	public void Unfocus()
-	{
-		isFocused = false;
+    public void Unfocus()
+    {
+        isFocused = false;
         transform.DOKill();
         transform.DOScale(1.0f * Vector3.one, 0.5f);
 
-		canvas.DOKill ();
-		canvas.DOFade (0f, 0.5f);
+        canvas.DOKill();
+        canvas.DOFade(0f, 0.5f);
+        canvas.interactable = false;
     }
 
     static string GetCoordinate(int index)
-	{
-		switch(index)
-		{
-		case 0:
-			return "x"; break;
-		case 1:
-			return "y"; break;
-		case 2:
-			return "z"; break;
-		default:
-			return "w"; break;
-		}
-	}
+    {
+        switch (index)
+        {
+            case 0:
+                return "x"; break;
+            case 1:
+                return "y"; break;
+            case 2:
+                return "z"; break;
+            default:
+                return "w"; break;
+        }
+    }
 
-	Vector3 initialSubEuler = Vector3.zero;
-	Vector3 initialEuler = Vector3.zero;
+    Vector3 initialSubEuler = Vector3.zero;
+    Vector3 initialEuler = Vector3.zero;
 
-	void Update () 
-	{
-		if (IsFocused && Controller.IsTargeting)
-		{
-			if (initialSubEuler == Vector3.zero)
-			{
-				initialSubEuler = InputRemoute.InputPacket.Gyroscope.Attitude.eulerAngles;
-				initialEuler = transform.eulerAngles;
-			}
+    void Update()
+    {
+        if (IsFocused && Controller.IsTargeting)
+        {
+            if (initialSubEuler == Vector3.zero)
+            {
+                initialSubEuler = InputRemoute.InputPacket.Gyroscope.Attitude.eulerAngles;
+                initialEuler = transform.eulerAngles;
+            }
 
-			transform.eulerAngles = initialEuler + InputRemoute.InputPacket.Gyroscope.Attitude.eulerAngles - initialSubEuler;
-		}
-		else if(initialSubEuler != Vector3.zero)
-			initialSubEuler = Vector3.zero;
-	}
+            transform.eulerAngles = initialEuler + InputRemoute.InputPacket.Gyroscope.Attitude.eulerAngles - initialSubEuler;
+        }
+        else if (initialSubEuler != Vector3.zero)
+            initialSubEuler = Vector3.zero;
+    }
 
+    public void OnEnable()
+    {
+
+    }
+
+    public void OnDisable()
+    {
+
+    }
 }
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        Controller.LeftSwipeEvent += SelectLeft;
+        Controller.RightSwipeEvent += SelectRight;
+    }
+
+    public void OnDisable()
+    {
+        Controller.LeftSwipeEvent -= SelectLeft;
+        Controller.RightSwipeEvent -= SelectRight;
+    }
+
+
+    private void SelectLeft()
+    {
+        GameObject current = EventSystem.current.currentSelectedGameObject;
+        if (current != null)
+        {
+            Selectable left = current.GetComponent<Selectable>().FindSelectableOnLeft();
+            if (left != null)
+            {
+                EventSystem.current.SetSelectedGameObject(left.gameObject);
+            }
+        }
+    }
+
+    private void SelectRight()
+    {
+        GameObject current = EventSystem.current.currentSelectedGameObject;
+        if (current != null)
+        {
+            Selectable left = current.GetComponent<Selectable>().FindSelectableOnLeft();
+            if (left != null)
+            {
+                EventSystem.current.SetSelectedGameObject(left.gameObject);
+            }
+        }
+    }
+
+
+}
